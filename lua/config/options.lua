9f58c9e7da2e5bbcfde195f96d1ba58016b127bc
mishka mrelashvili <mishka.mrelashvili@gmail.com>
2025-11-16 20:24:12 +0400

    myvim_1.1

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,3 +3,9 @@
 -- Add any additional options here
 -- then you need to set the option below.
 vim.g.lazyvim_picker = "telescope"
+
+-- Disable relative line numbers
+vim.opt.relativenumber = false
+
+-- Enable static line numbers
+vim.opt.number = true
--- lua/plugins/colorschema.lua
+++ lua/plugins/colorschema.lua
@@ -1,11 +1,21 @@
 -- Using Lazy
+-- return {
+--   "navarasu/onedark.nvim",
+--   priority = 1000, -- make sure to load this before all the other start plugins
+--   config = function()
+--     require("onedark").setup({
+--       style = "cool",
+--     })
+--     require("onedark").load()
+--   end,
+-- }
+
 return {
-  "navarasu/onedark.nvim",
-  priority = 1000, -- make sure to load this before all the other start plugins
-  config = function()
-    require("onedark").setup({
-      style = "cool",
-    })
-    require("onedark").load()
-  end,
+  {
+    "olimorris/onedarkpro.nvim",
+    priority = 1000, -- load first
+    config = function()
+      vim.cmd("colorscheme onedark") -- or onedark_vivid, onelight, etc.
+    end,
+  },
 }
